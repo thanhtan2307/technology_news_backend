a5a13d7f1a06d05d15ef89ce46d36bca1d07f747
thanhtan2307 <71721020+thanhtan2307@users.noreply.github.com>
2021-05-10 15:10:28 +0700

    Update index.js

--- src/apis/routes/index.js
+++ src/apis/routes/index.js
@@ -6,7 +6,7 @@ const meRouter = require('./me')
 const authRouter = require('./auth')
 const searchRouter = require('./search')
 const categoryRouter = require('./category')
-const newsRouter = require('./news')
+//const newsRouter = require('./news')
 
 const route = (app) => {
     app.use('/api/site', siteRouter)
