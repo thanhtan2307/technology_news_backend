d64426c2a7992ffa500f445da94b5148df2ae718
thanhtan2307 <71721020+thanhtan2307@users.noreply.github.com>
2021-05-10 13:56:11 +0700

    Update index.js

--- src/apis/routes/index.js
+++ src/apis/routes/index.js
@@ -6,6 +6,7 @@ const meRouter = require('./me')
 const authRouter = require('./auth')
 const searchRouter = require('./search')
 const categoryRouter = require('./category')
+const newsRouter = require('./news')
 
 const route = (app) => {
     app.use('/api/site', siteRouter)
@@ -16,6 +17,7 @@ const route = (app) => {
     app.use('/api/auth', authRouter)
     app.use('/api/search', searchRouter)
     app.use('/api/categories', categoryRouter)
+    app.use('/api/news', newsRouter)
 }
 
 module.exports = route
